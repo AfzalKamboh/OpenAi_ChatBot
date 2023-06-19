de73c526e60d16ffa8b96e5c369cd7b145cb85e7
AfzalKamboh <afzalkam011@gmail.com>
2023-06-19 10:45:07 +0500

    Working in StandardMessageForm File

--- client/src/components/customMessageForm/StandardMessageForm.jsx
+++ client/src/components/customMessageForm/StandardMessageForm.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { XMarkIcon } from '@heroicons/react/24/solid' 
+
 
 const StandardMessageForm = () => {
+  const [message,setMessage] = useState("");
+  const [attachment,setAttachment] = useState("");
+  const [preview,setPreview] = useState("");
+
+
+  const handleChange = (e) => setMessage(e.target.value);
+   
   return (
-    <div>
-      Standard Message Form
+    <div className='message-form-container'>
+      {preview && (
+        <div className='message-form-preview'>
+          <img alt="message-form-preview"
+          className='message-form-preview-image'
+          src={preview}
+          onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon 
+            className='message-form-icon-x'
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
+        </div>
+      )}
+      <div className='message-form'>
+        <div className='message-form-input-container'>
+          <input className='message-form-input' 
+          type="text" 
+          value={message}
+          onChange={handleChange}
+          placeholder='Send a message...'
+          />
+        </div>
+        <div className='message-form-icons'>
+            
+        </div>
+
+      </div>
+ 
     </div>
   )
 }
